350d4a59e586e9c4a22d35c687208ad410f03c54
Alex Trew <trew13690@gmail.com>
2019-07-01 16:50:08 -0700

    Added webpack configuration

--- src/app.js
+++ src/app.js
@@ -1,2 +1,18 @@
-imports 'React' from 'react'
+import React from "react";
+import ReactDom from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
 
+function App(props) {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+
+ReactDom.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,39 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+var path = require("path")
+
 module.exports = {
+    entry: ["./src/app.js"],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "source_bundle.js",
+        publicPath: "/"
+
+    },
     module: {
         rules:[
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use:{
-                    loader: "babel-loader"
+                loaders: ["babel-loader"]
+            } , 
+            {
+            test: /\.html$/,
+            use: [
+                {
+                loader: "html-loader"
                 }
-            }
-        ]
-    }
-}
+            ]
+        
+    },
+    ]
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    plugins:[
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
