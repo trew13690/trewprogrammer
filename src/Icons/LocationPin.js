445796f97c91c5e3185c576f96f2e45319bc94e8
Alex <trew13690@gmail.com>
2019-07-16 14:48:57 -0700

    Initial layout and style on header

---
+++ src/Icons/LocationPin.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SvgLocationPin = props => (
+  <svg width={20} height={20} {...props}>
+    <path d="M10 2.009c-2.762 0-5 2.229-5 4.99 0 4.774 5 11 5 11s5-6.227 5-11c0-2.76-2.238-4.99-5-4.99zm0 7.751a2.7 2.7 0 110-5.4 2.7 2.7 0 010 5.4z" />
+  </svg>
+);
+
+export default SvgLocationPin;
---
+++ src/Icons/Star.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SvgStar = props => (
+  <svg width={20} height={20} {...props}>
+    <path d="M10 1.3l2.388 6.722H18.8l-5.232 3.948 1.871 6.928L10 14.744l-5.438 4.154 1.87-6.928-5.233-3.948h6.412L10 1.3z" />
+  </svg>
+);
+
+export default SvgStar;
