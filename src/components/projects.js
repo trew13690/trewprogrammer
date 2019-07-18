f65ae0307a1ad271e359186c11221357b8fa7c84
Alex <trew13690@gmail.com>
2019-07-17 18:28:04 -0700

    Passover on Project Section

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,12 @@
 import React from "react";
+import NaturePhoto from '../assets/img/NatureScreenShot.jpg';
+import TrelloPhoto from '../assets/img/Trello.jpg';
+import InProgress from '../assets/img/InProgress.jpg';
+import  {Link} from 'react-router-dom';
+import Github from '../Icons/Github';
+import Live from '../Icons/Globe';
+
+
 export default function Projects() {
   
   return (
@@ -9,42 +17,57 @@ export default function Projects() {
 
         <div className="project-section_container">
           <div className="project-section_container-project">
-              <img src="" alt="" className="project-section_container-project-img"/>
+          <div className="project-section_container-project-top">
+              <img src={NaturePhoto} alt="" className="project-section_container-project-img"/>
               <h4 className="header-4">
                 Nature Demo 
               </h4>
+              </div>
+              
               <p className="project-section_container-project-description">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque iusto saepe repellendus officiis eius. Aut, itaque? Repudiandae sunt rerum quas dolor consequatur laudantium eum rem assumenda, soluta a ut repellat!
               </p>
               <div className="project-section_container-project-buttonContainer">
-              <button className="project-section_container-project-button">Live</button>
-                <button className="project-section_container-project-button">Code</button>
+             
+             
+              <Live height='25' width='25' fill='#000'className="project-section_container-project-button"/>
+              
+              <a href="https://github.com/trew13690/NaturePortfolioSample" target="_blank">
+               <Github fill='#000' width='25' height='25' className="project-section_container-project-button" />
+               </a>
                 </div>
           </div>
           <div className="project-section_container-project">
-            <img src="" alt="" className="project-section_container-project-img"/>
+           <div className="project-section_container-project-top">
+            <img src={TrelloPhoto} alt="" className="project-section_container-project-img"/>
               <h4 className="header-4">
                 Trello  Demo
               </h4>
+              </div>
               <p className="project-section_container-project-description">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque iusto saepe repellendus officiis eius. Aut, itaque? Repudiandae sunt rerum quas dolor consequatur laudantium eum rem assumenda, soluta a ut repellat!
               </p>
               <div className="project-section_container-project-buttonContainer">
-              <button className="project-section_container-project-button">Live</button>
-                <button className="project-section_container-project-button">Code</button>
+              <Live className="project-section_container-project-button"/>
+              <a href="https://github.com/trew13690/Trello" target="_blank">
+                <Github className="project-section_container-project-button"/>
+                </a>
                 </div>
           </div>
           <div className="project-section_container-project">
-            <img src="" alt="" className="project-section_container-project-img"/>
+           <div className="project-section_container-project-top">
+            <img src={InProgress} alt="" className="project-section_container-project-img"/>
               <h4 className="header-4">
                 Ark Tribe Manager
               </h4>
+              </div>
               <p className="project-section_container-project-description">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque iusto saepe repellendus officiis eius. Aut, itaque? Repudiandae sunt rerum quas dolor consequatur laudantium eum rem assumenda, soluta a ut repellat!
               </p>
               <div className="project-section_container-project-buttonContainer">
-              <button className="project-section_container-project-button">Live</button>
-                <button className="project-section_container-project-button">Code</button>
+              <Live className="project-section_container-project-button"/>
+              
+                <Github className="project-section_container-project-button"/>
                 </div>
           </div>
         </div>
