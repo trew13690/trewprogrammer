8ef54b644415b323910481a79759c04644f5c506
Alex <trew13690@gmail.com>
2019-09-03 22:43:50 -0700

    Changed Version Number

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ export default function footer(){
     return(
 
         <div  className="footer">
-        <small className="text-muted footer-copyright">Alex Trew;  TrewDev &copy; Version 2.0.0; Not Mobile Responsive till 2.2.0</small>
+        <small className="text-muted footer-copyright">Alex Trew;  TrewDev &copy; Version 2.2.0; Not Mobile Responsive till 2.2.0</small>
         <div className="footer-contact">
            <a href="https://github.com/trew13690"  target='_blank'><Github className="footer-contact-item"/></a>
            <a href="https://gitlab.com/redtrew" target='_blank'>
